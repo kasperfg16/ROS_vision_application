cc63077d26e6a4ead1d72d301b874281bf4292d7
kasperfg16 <56404081+kasperfg16@users.noreply.github.com>
2021-11-17 10:28:37 +0100

    Delete devel_isolated directory

--- devel_isolated/env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin.builder Python module
-
-/home/ubuntu/p5_project/devel_isolated/vision_application_movit_config/env.sh "$@"
--- devel_isolated/setup.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin.builder Python module
-
-. "/home/ubuntu/p5_project/devel_isolated/vision_application_movit_config/setup.bash"
