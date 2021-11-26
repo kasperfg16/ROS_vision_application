41e7c96cd8de008901cbd81dd2794bada63afb61
MadsRossen <56363524+MadsRossen@users.noreply.github.com>
2021-11-26 00:48:15 -0800

    Action server running

--- src/messages/src/action_server.py
+++ src/messages/src/action_server.py
@@ -1,20 +1,19 @@
  #! /usr/bin/env python
-
 from __future__ import print_function
-
 from threading import main_thread
-from genpy import message
-
 import rospy
 import actionlib
-import messages.msg 
+import messages.msg
 
 class robotposition(object):
-    _feedback = messages.msg.posActionFeedback
-    _result = messages.msg.posResult
+    # Uesd for feedback of result and goal if needed
+    _feedback = messages.msg.posFeedback()
+    _result = messages.msg.posResult()
+    _goal = messages.msg.posGoal()
 
     def __init__(self, name):
         self._action_name = name
+        # Create and start the action server
         self._as = actionlib.SimpleActionServer(self._action_name, messages.msg.posAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
         
@@ -22,28 +21,19 @@ class robotposition(object):
         r = rospy.Rate(1)
         success = True
 
-        self._feedback.sequence = []
-        self._feedback.sequence.append(0)
-        self._feedback.sequence.append(1)
-
-        rospy.loginfo('%s: Executing %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
-
-        for i in range(1, goal.order):
-
-            rospy.loginfo('%s: Preempted' % self._action_name)
-            self._as.set_preempted
-            success = False
-            break
-        self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
-
-        self._as.publish_feedback(self._feedback)
-
-        r.sleep
+        # Print the position of recived goal
+        print("Position: ")
+        print("x: ", goal.x)
+        print("y: ", goal.y)
+        print("z: ", goal.z)
+        print("rotx: ", goal.rotx)
+        print("roty: ", goal.roty)
+        print("rotz: ", goal.rotz)
 
         if success:
-            self._result.sequence = self._feedback.sequence
             rospy.loginfo('%s: Succeeded' % self._action_name)
-            self._as.set_succeeded(self._result)
+
+        return goal.x, goal.y, goal.z, goal.rotx, goal.roty, goal.rotz
 
 if __name__ == '__main__':
     rospy.init_node('Robot_position_msg')
