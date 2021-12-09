9f2f3733524fbd390ddfc7729a654bbcbcf50160
kasperfg16 <56404081+kasperfg16@users.noreply.github.com>
2021-12-09 11:30:08 +0100

    code cleanup

--- src/robot_handler/src/run_robot_handler.py
+++ src/robot_handler/src/run_robot_handler.py
@@ -62,33 +62,23 @@ class robotInterface(object):
 
         super(robotInterface, self).__init__()
 
-        # BEGIN_SUB_TUTORIAL setup
         ##
         # First initialize `moveit_commander`_ and a `rospy`_ node:
         moveit_commander.roscpp_initialize(sys.argv)
-        # rospy.init_node('move_group_python_interface',
-        #                anonymous=True)
 
         # Instantiate a `RobotCommander`_ object. This object is the outer-level interface to
         # the robot:
         robot = moveit_commander.RobotCommander()
-        
 
         # Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
         # to the world surrounding the robot:
         scene = moveit_commander.PlanningSceneInterface()
 
         # Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-        # to one group of joints.  In this case the group is the joints in the Panda
-        # arm so we set ``group_name = panda_arm``. If you are using a different robot,
+        # to one group of joints. In this case the group is the joints in one of the ur5 robots
+        # arm so if we want to control the UR5 with the Smart light we set ``group_name = `ur5_cam`.
         # you should change this value to the name of your robot arm planning group.
-        # This interface can be used to plan and execute motions on the Panda:
 
-        #ur5_robot_name = "ur5_cam"
-        #ur5_robot_name = "ur5_light_bar"
-
-
-   
         group = moveit_commander.MoveGroupCommander(group_name)
         
         group.set_num_planning_attempts(100)
@@ -340,7 +330,7 @@ if __name__ == '__main__':
     posBacklight = [0.367, 0.120, 0]
     robot_cam.add_static_scene(posBacklight)
     server_name = action_server(rospy.get_name(), robot_cam, robot_light)
-    # Create and start the action server
+    # Create and start the action server and execute server program if message is recived
     rospy.spin
 
 
