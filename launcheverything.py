65ca5cdafe66a95337b71f4f6305235f630b0156
MadsRossen <56363524+MadsRossen@users.noreply.github.com>
2021-12-10 01:16:26 -0800

    Update launchfiler.py

--- launcheverything.py
+++ launcheverything.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import time
 
+
 #path = " cd Dokumenter/ROB5_project/ROS_vision_application"
 
 # os.path.join(os.getenv(''))
@@ -21,12 +22,12 @@ for root, dirs, files in os.walk("/home"):
 
 subprocess.Popen("source " + path + "/devel/setup.bash", shell=True, executable='/bin/bash')
 os.system('cd ' + str(path))
-subprocess.Popen("roslaunch ur_description vision_application_bringup.launch", shell=True)
-time.sleep(7)
-subprocess.Popen("roslaunch vision_application_movit_config vision_application_moveit_planning_execution.launch", shell=True)
-subprocess.Popen("roslaunch vision_application_movit_config moveit_rviz.launch rviz_config:=$(rospack find vision_application_movit_config)/launch/moveit.rviz", shell=True)
-subprocess.Popen("roslaunch rosbridge_server rosbridge_websocket.launch", shell=True)
-subprocess.Popen("python3 " + path + "/run_robot_handler.py", shell=True)
+output = subprocess.run("roslaunch ur_description vision_application_bringup.launch", shell=True).stdout
+if output == "Robot connected to reverse interface. Ready to receive control commands.":
+    subprocess.Popen("roslaunch vision_application_movit_config vision_application_moveit_planning_execution.launch", shell=True)
+    subprocess.Popen("roslaunch vision_application_movit_config moveit_rviz.launch rviz_config:=$(rospack find vision_application_movit_config)/launch/moveit.rviz", shell=True)
+    subprocess.Popen("roslaunch rosbridge_server rosbridge_websocket.launch", shell=True)
+    subprocess.Popen("python3 " + path + "/run_robot_handler.py", shell=True)
 
-subprocess.Popen("python3 " + str(path2), shell=True)
+    subprocess.Popen("python3 " + str(path2), shell=True)
 
